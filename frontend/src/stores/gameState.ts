95a3bff039ec2360cda7c6b0ad4a38198c548583
Mattias Aabmets <mattias.aabmets@tietoevry.com>
2025-10-11 17:08:32 +0300

    Added pinia store for game state

---
+++ frontend/src/stores/gameState.ts
@@ -0,0 +1,23 @@
+import { defineStore } from 'pinia'
+
+export interface Game {
+  uuid: string
+  timestamp: string
+  username: string
+  gameId: string
+  lives: number
+  gold: number
+  level: number
+  score: number
+  highScore: number
+  turn: number
+}
+
+export const useGameStore = defineStore('game-state', {
+  state: () => ({ game: null }),
+  actions: {
+    setGame(payload: Game | null) {
+      this.game = payload
+    }
+  }
+})
