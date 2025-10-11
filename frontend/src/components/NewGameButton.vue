6d48da224252f7a1fcd5c0bfe219bfa159613183
Mattias Aabmets <mattias.aabmets@tietoevry.com>
2025-10-11 16:46:06 +0300

    New game button now surfaces player name input and submit button

--- frontend/src/components/NewGameButton.vue
+++ frontend/src/components/NewGameButton.vue
@@ -5,14 +5,12 @@ import { useMotion } from '@vueuse/motion'
 interface Props {
   color?: string
   size?: string
-  disabled?: boolean
   shrinkDurationMs?: number
 }
 
 const props = withDefaults(defineProps<Props>(), {
   color: 'primary',
   size: 'large',
-  disabled: false,
   shrinkDurationMs: 400,
 })
 
@@ -21,11 +19,27 @@ const emit = defineEmits<{
 }>()
 
 const btnRef = ref<HTMLElement | null>(null)
+const formRef = ref<HTMLElement | null>(null)
+const showingForm = ref(false)
+const playerName = ref('')
 
-const motion = useMotion(btnRef, {
-  initial: {
-    scale: 1.15,
-  },
+const shrinkMotionConfig = {
+  shrink: {
+    scale: 0,
+    transition: {
+      duration: props.shrinkDurationMs,
+      easing: 'easeIn'
+    }
+  }
+}
+
+const formMotion = useMotion(formRef, {
+  initial: { scale: 1 },
+  ...shrinkMotionConfig,
+})
+
+const buttonMotion = useMotion(btnRef, {
+  initial: { scale: 1.15 },
   enter: {
     scale: [1, 1.15],
     transition: {
@@ -35,32 +49,59 @@ const motion = useMotion(btnRef, {
       easing: 'easeInOut'
     },
   },
-  shrink: {
-    scale: 0,
-    transition: {
-      duration: props.shrinkDurationMs,
-      easing: 'easeIn'
-    },
-  },
+  ...shrinkMotionConfig,
 })
 
-const onClick = () => {
-  motion.apply('shrink')
+async function onClick() {
+  await buttonMotion.apply('shrink')
+  showingForm.value = true
+}
+
+async function onSubmit() {
+  await formMotion.apply('shrink')
   emit('click')
 }
 </script>
 
 <template>
-  <div ref="btnRef">
+  <div v-show="!showingForm" ref="btnRef" class="space">
     <v-btn
       :color="color"
       :size="size"
-      :disabled="disabled"
       @click="onClick">
         <slot>New Game</slot>
     </v-btn>
   </div>
+  <v-scale-transition>
+    <v-sheet
+      v-if="showingForm"
+      ref="formRef"
+      :height="44"
+      :width="300"
+      class="space transparent"
+      rounded>
+      <v-row>
+        <v-text-field
+          v-model="playerName"
+          label="Enter Player Name (Optional)"
+          variant="solo"
+          clearable
+          autofocus
+          @keyup.enter="onSubmit"
+        />
+        <v-btn color="primary" :height="56" @click="onSubmit">
+          PLAY
+        </v-btn>
+      </v-row>
+    </v-sheet>
+  </v-scale-transition>
 </template>
 
 <style scoped>
+.space {
+  margin: 20px 0 0 20px;
+}
+.transparent {
+  background-color: rgba(0, 0, 0, 0);
+}
 </style>
