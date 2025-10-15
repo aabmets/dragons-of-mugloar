1c9af0b6bbddea6d7ced8953c98fae8ccc751aee
Mattias Aabmets <mattias.aabmets@gmail.com>
2025-10-15 13:04:46 +0300

    Shortened skip a turn button info due to layout issues

--- frontend/src/components/Shop.vue
+++ frontend/src/components/Shop.vue
@@ -55,7 +55,7 @@ import SkipTurnButton from "@/components/SkipTurnButton.vue"
             ⚠️
           </span>
           <span style="font-size: 0.8rem;">
-            Skipping a turn also updates reputation
+            Also updates reputation:
           </span>
         </div>
         <SkipTurnButton />
